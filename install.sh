4704cbe3d114e2825a454afa0b4278c87b94f136
Qali3 <ltwod2@proton.me>
2026-03-02 16:33:14 +0000

    Refactor install.sh for improved structure and clarity
    Refactor install script to use functions for user input and improve readability. Update variable handling for disk and partition names.

--- install.sh
+++ install.sh
@@ -1,88 +1,102 @@
-#!/bin/sh
+#!/bin/bash
 
 setfont ter-132b
 
-SURE='0'
+setvar() {
+        local answer
 
-while [ !$SURE ]; do
-	echo 'Please, enter your username: '
-	read USERNAME
-	clear
-	echo "Is {$USERNAME} correct?"
-	read SURE
-	if 
+        while true; do
+                read -p "Please, enter $1: " $2
+                read -p 'Continue? [Y/n]' answer
+                answer=${answer:-Y}
+                case $answer in
+                        [Yy]*) break;;
+                        [Nn]*) continue;;
+                        *) echo 'Please, enter y or n.';;
+                esac
+        done
+}
+
+setvar 'your username' username
+setvar 'your hostname' hostname
+setvar 'your Region' region
+setvar 'your City' city
+
+while true; do
+        local checkPassword
+
+        read -psr 'Please, enter your password: ' password
+        read -psr 'Please, reenter your password: ' checkPassword
+
+        clear
+
+        if [[ $password == $checkPassword ]]; then
+                break
+        fi
+
+        echo 'Passwords do not match.'
+        sleep 1
 done
-echo 'Please, enter a name for your computer: '
-read COMPUTERNAME
 
-echo 'Please, enter Region/City: '
-read TIME
+lsblk
 
-PASSWORD='1'
-PASSWORD2='0'
+setvar 'a path to your disk (all of your data on that disk will be erased)' disk
+diskPart1="${disk}p1"
+diskPart2="${disk}p2"
 
-while [ $PASSWORD != $PASSWORD2 ]; do
-	echo 'Please, enter your password: '
-	read -sr PASSWORD
+parted --script $disk mklabel gpt mkpart primary fat32 1MiB 257MiB mkpart primary ext4 257MiB 100% set 1 esp on
 
-	echo 'Please, enter your password again: '
-	read -sr PASSWORD2
-	clear
-done
+mkfs.vfat $diskPart1
+mkfs.ext4 $diskPart2
 
-DISK='/dev/nvme0n1'
-PART1='/dev/nvme0n1p1'
-PART2='/dev/nvme0n1p2'
-
-parted --script $DISK \
-	mklabel gpt \
-	mkpart primary fat32 1MiB 257MiB \
-	mkpart primary ext4 257MiB 100% \
-	set 1 esp on
-
-mkfs.vfat $PART1
-mkfs.ext4 $PART2
-
-mount $PART2 /mnt
+mount $diskPart2 /mnt
 mkdir /mnt/boot
-mount $PART1 /mnt/boot
+mount $diskPart1 /mnt/boot
 
-pacstrap -K /mnt linux-zen linux-firmware base efibootmgr reflector neovim base-devel git fish
+pacstrap -K /mnt linux-zen linux-firmware base efibootmgr neovim base-devel git fish
 
 genfstab -U /mnt > /mnt/etc/fstab
 
 arch-chroot /mnt bash <<EOF
-ln -sf /usr/share/zoneinfo/$TIME /etc/localtime
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
 hwclock --systohc
 
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
-echo $COMPUTERNAME > /etc/hostname
+echo $hostname > /etc/hostname
 
-echo '127.0.0.1 localhost' > /etc/hosts
-echo '::1 localhost' >> /etc/hosts
-echo "127.0.1.1 $COMPUTERNAME.localdomain $COMPUTERNAME" >> /etc/hosts
+cat <<EOT > /etc/hosts
+127.0.0.1 localhost
+::1 localhost
+127.0.1.1 $hostname.localdomain $hostname
+EOT
 
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-echo 'DNS=9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net' > /etc/systemd/resolved.conf
-echo 'DNSOverTLS=yes' >> /etc/systemd/resolved.conf
-echo 'Domains=~.' >> /etc/systemd/resolved.conf
+cat <<EOT > /etc/systemd/resolved.conf
+DNS=9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net
+DNSOverTLS=yes
+Domains=~.
+EOT
 
-echo '[Match]' > /etc/systemd/network/20-wired.network
-echo 'Name=enp10s0' >> /etc/systemd/network/20-wired.network
-echo '[Network]' >> /etc/systemd/network/20-wired.network
-echo 'DHCP=yes' >> /etc/systemd/network/20-wired.network
+cat <<EOT > /etc/systemd/network/20-wired.network
+[Match]
+Name=enp10s0
+[Network]
+DHCP=yes
+EOT
 
 systemctl enable systemd-networkd systemd-resolved
 
-useradd -m -G wheel -s /bin/bash $USERNAME
+useradd -m -G wheel -s /bin/bash $username
 
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo "root:$PASSWORD" | chpasswd
+echo "$username:$password" | chpasswd
+echo "root:$password" | chpasswd
 echo '%wheel ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
-efibootmgr --create --disk $DISK --part 1 --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode "root=PARTUUID=$(blkid -s PARTUUID -o value $PART2) initrd=/initramfs-linux-zen.img"
+efibootmgr --create --disk $disk --part 1 --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode "root=PARTUUID=$(blkid -s PARTUUID -o value $diskPart2) initrd=/initramfs-linux-zen.img"
+
+pacman -Rns efibootmgr
 EOF
